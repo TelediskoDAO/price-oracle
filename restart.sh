99d2f3d9af172478474da7d860d0759ffff6f5d9
Nicola Miotto <miotto@posteo.de>
2023-01-05 15:23:54 +0100

    add monitor

--- restart.sh
+++ restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source .env
+source environment
 
 mkdir -p logs
 
 ps -ef | grep oracle.py | head -1 | awk '{print $2}' | xargs kill || true
-#ps -ef | grep monitor.py | head -1 | awk '{print $2}' | xargs kill
+ps -ef | grep monitor.py | head -1 | awk '{print $2}' | xargs kill
 
 nohup python3 src/oracle.py &
-#nohup flask run &
+nohup flask run &
---
+++ src/monitor.py
@@ -0,0 +1,17 @@
+from datetime import timedelta
+from time import time
+from flask import Flask
+import os
+
+INFO_LOG = os.environ["INFO_LOG"]
+
+app = Flask(__name__)
+
+@app.route("/")
+def check():
+    with open(INFO_LOG, "r") as logs:
+        last_update = int(logs.read().split("\n")[-1].split("\t")[0])
+        if time.time() > last_update + 60 * 12:
+            return "error", 500
+        else:
+            return "ok"
