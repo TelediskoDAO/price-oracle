f5005548de4342dbbb495b44d0da2bb7f2aae02c
Nicola Miotto <miotto@posteo.de>
2023-01-05 15:39:19 +0100

    add debug

--- restart.sh
+++ restart.sh
@@ -7,7 +7,5 @@ mkdir -p logs
 ps -ef | grep oracle.py | head -1 | awk '{print $2}' | xargs kill || true
 ps -ef | grep "flask run -p $FLASK_PORT" | head -1 | awk '{print $2}' | xargs kill || true
 
-sleep 3
-
 nohup python3 src/oracle.py &
 nohup flask run -p $FLASK_PORT &
--- src/monitor.py
+++ src/monitor.py
@@ -1,6 +1,8 @@
+from distutils.log import INFO
 from time import time
 from flask import Flask
 import os
+import logging
 
 INFO_LOG = os.environ["INFO_LOG"]
 
@@ -8,6 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def check():
+    logging.error(INFO_LOG)
     with open(INFO_LOG, "r") as logs:
         last_update = int(logs.read().split("\n")[-1].split("\t")[0])
         if time() > last_update + 60 * 12:
