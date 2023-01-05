032a7de2177b74fe4943615de3090c116790bd8f
Nicola Miotto <miotto@posteo.de>
2023-01-05 15:10:19 +0100

    add env sourcing

--- restart.sh
+++ restart.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#export FLASK_APP=./src/monitor.py
-export INFO_LOG=./logs/info.logs
-export ERROR_LOG=./logs/error.logs
+source .env
 
 mkdir -p logs
 
-ps -ef | grep oracle.py | head -1 | awk '{print $2}' | xargs kill
+ps -ef | grep oracle.py | head -1 | awk '{print $2}' | xargs kill || true
 #ps -ef | grep monitor.py | head -1 | awk '{print $2}' | xargs kill
 
 nohup python3 src/oracle.py &
