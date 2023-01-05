83e70b0523739ea512c771d8b1b51a0ec2711331
Nicola Miotto <miotto@posteo.de>
2023-01-05 15:27:51 +0100

    add custom flask port

--- restart.sh
+++ restart.sh
@@ -5,7 +5,7 @@ source environment
 mkdir -p logs
 
 ps -ef | grep oracle.py | head -1 | awk '{print $2}' | xargs kill || true
-ps -ef | grep monitor.py | head -1 | awk '{print $2}' | xargs kill
+ps -ef | grep monitor.py | head -1 | awk '{print $2}' | xargs kill || true
 
 nohup python3 src/oracle.py &
-nohup flask run &
+nohup flask run -p $FLASK_PORT &
