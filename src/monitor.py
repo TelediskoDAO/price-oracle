f9cdedb2cbda86bb0ae6da405265f195c3f558d7
Nicola Miotto <miotto@posteo.de>
2023-01-05 15:42:21 +0100

    fix line reading

--- src/monitor.py
+++ src/monitor.py
@@ -2,7 +2,6 @@ from distutils.log import INFO
 from time import time
 from flask import Flask
 import os
-import logging
 
 INFO_LOG = os.environ["INFO_LOG"]
 
@@ -10,9 +9,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def check():
-    logging.error(INFO_LOG)
     with open(INFO_LOG, "r") as logs:
-        last_update = int(logs.read().split("\n")[-1].split("\t")[0])
+        lines = list(filter(lambda x: x, logs.read().split("\n")))
+        last_update = int(lines[-1].split("\t")[0])
         if time() > last_update + 60 * 12:
             return "error", 500
         else:
